ce7e3f1b2d61aff4c7f1b8be4bc0ad5947c7284c
sahibibadov <117587344+sahibibadov@users.noreply.github.com>
2024-07-01 13:07:19 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1">
       <Hero />
-      <Resume key={Math.random()} />
+      <Resume/>
       <Skills />
       <Project />
     </div>
