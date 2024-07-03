fefac0bee5c95c56e268c318ead0ef7b8eca9083
sahibibadov <117587344+sahibibadov@users.noreply.github.com>
2024-07-03 10:50:14 +0400

    Update footer.tsx date format

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <footer className="border-t ~mt-8/12">
         <Container className="flex flex-col md:flex-row items-center md:justify-between gap-1 ~p-3/6 backdrop-blur">
           <p className="~text-sm/base  tracking-normal">
-            Copyright © 2024. Made with by Sahib Ibadov
+            Copyright © {new Date().getFullYear()}. Made with by Sahib Ibadov
           </p>
           {/* social icons */}
           <SocialIcons />
