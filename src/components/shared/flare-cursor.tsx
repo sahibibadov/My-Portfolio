fe13fb0c1fd1c29f60f020659641d74b1cd1124c
sahibibadov <117587344+sahibibadov@users.noreply.github.com>
2024-08-23 16:05:15 +0400

    Update flare-cursor.tsx

--- src/components/shared/flare-cursor.tsx
+++ src/components/shared/flare-cursor.tsx
@@ -1,5 +1,88 @@
 "use client";
 
+import React, { useCallback, useState } from "react";
+import { AnimatePresence, motion, useMotionValue, useSpring, Variants } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+interface CursorProps {}
+
+const FramerCursor: React.FC<CursorProps> = () => {
+  const [isPointer, setIsPointer] = useState<boolean>(false);
+  const cursorX = useMotionValue<number>(-100);
+  const cursorY = useMotionValue<number>(-100);
+
+  const springConfig = { damping: 100, stiffness: 1000 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  const moveCursor = useCallback(
+    (e: MouseEvent) => {
+      cursorX.set(e.clientX - 16);
+      cursorY.set(e.clientY - 16);
+
+      // Check if the cursor is over specific elements
+      const elements = document.elementsFromPoint(e.clientX, e.clientY);
+      const isOverTargetElement = elements.some(
+        (element) =>
+          ["h1", "h2", "h3", "button", "a", "input", "label"].includes(element.tagName.toLowerCase()) ||
+          element.hasAttribute("data-cursor"),
+      );
+
+      setIsPointer(isOverTargetElement);
+    },
+    [],
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, [moveCursor]);
+
+  const variants: Variants = {
+    normal: {
+      scale: 1,
+      transition: {
+        duration: 0.2,
+        ease: "easeOut",
+      },
+    },
+    pointer: {
+      scale: 0.5,
+      transition: {
+        duration: 0.2,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  return (
+      <motion.div
+        className="pointer-events-none fixed z-[10001] hidden md:block"
+        initial="normal"
+        animate={isPointer ? "pointer" : "normal"}
+        exit="normal"
+        transition={{ duration: 0.2 }}
+        variants={variants}
+        style={{
+          translateX: cursorXSpring,
+          translateY: cursorYSpring,
+        }}
+      >
+        <div
+          className={cn(
+            "mix-blend-mode-difference bg-blend-mode-difference size-9 rounded-full border border-white/30  backdrop-blur-sm ",
+          )}
+        />
+      </motion.div>
+  );
+};
+
+export default FramerCursor;
+/*"use client";
+
 import { cn } from "@/lib/utils";
 import { useState, useEffect, useCallback, useMemo } from "react";
 
@@ -42,4 +125,4 @@ const FramerCursor = () => {
   );
 };
 
-export default FramerCursor;
+export default FramerCursor;*/
